901534fd6686126e3f0db89edf2cb189185ce930
heyzeusXc <joereidl@joereidlsmbp2.home>
2014-06-15 14:10:38 -0400

    Tested out scraping through all the seeds

--- newsmap seed.py
+++ newsmap seed.py
@@ -6,36 +6,44 @@ import csv
 import urllib2
 import re
 
+bin = []
+
 def seeder():
 	sites = ['The Guardian','New York Times','Washington Post','BBC','ABC','CBS','NBC','Reuters','CNN'] # list of the site names 
 	seeds = [] #holding list
-	with open(str(os.sys.path[0]) + '/News Map Seed List - Sheet1.csv','r') as seedlist:
-		data = csv.reader(seedlist)
-		for row in data:
-			if 'Site Name' in row or row[0] == '':
-				pass
-			else:
-				if row[0] in sites:				#checks if the site changes
-					holder = row[0]
-				if holder == row[0]:			#checks if it's the homepage
-					row[0] = 'Homepage'
-				item = [holder,row[0],row[1]] 	#sets the site name, page, and link
-				seeds.append(item) 				#adds the item to the seeds list
-		seedlist.close()
-		return seeds
+	try:
+		with open(str(os.sys.path[0]) + '/News Map Seed List - Sheet1.csv','r') as seedlist:
+			data = csv.reader(seedlist)
+			for row in data:
+				if 'Site Name' in row or row[0] == '':
+					pass
+				else:
+					if row[0] in sites:				#checks if the site changes
+						holder = row[0]
+					if holder == row[0]:			#checks if it's the homepage
+						row[0] = 'Homepage'
+					item = [holder,row[0],row[1]] 	#sets the site name, page, and link
+					seeds.append(item) 				#adds the item to the seeds list
+			seedlist.close()
+			return seeds
+	except:
+		print 'Seed list .csv not found'
 			
 seeds = seeder()
 
-def keycheck():
+def keymaker():
 	keys = []
-	with open(str(os.sys.path[0]) + '/Keyword List - Sheet1.csv','r') as keylist:
-		keywords = csv.reader(keylist)
-		for x in keywords:
-			keys.append(x[0])
-		keylist.close()
-		return keys
+	try:
+		with open(str(os.sys.path[0]) + '/Keyword List - Sheet1.csv','r') as keylist:
+			keywords = csv.reader(keylist)
+			for x in keywords:
+				keys.append(x[0])
+			keylist.close()
+			return keys
+	except:
+		print 'Keyword .csv file not found'
 		
-keys = keycheck()
+keys = keymaker()
 
 def scraper(s):
 	try:
@@ -46,11 +54,16 @@ def scraper(s):
 	except:
 		return 'Error loading site:' + s
 
-<<<<<<< HEAD
 for x in seeds:
 	for y in scraper(x[2]):
-		print y
-=======
-for x in sorted(links):
-	print x
->>>>>>> 4d480ee7d54bae489389f0bb30783f801b5fb386
+		for z in keys:
+			if z in y:
+				if y not in bin:
+					print z + ' found in ' + y
+					bin.append(y)
+		
+#for x in bin:
+#	print x
+
+	
+			
